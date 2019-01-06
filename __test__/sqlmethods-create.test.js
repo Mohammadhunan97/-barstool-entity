89799a6c6ebcd084dde189bcd39a6035d8c1facf
mchugtai <mchugtai@guildmortgage.net>
2019-01-06 02:36:13 -0800

    added more test coverage

--- __test__/sqlmethods-create.test.js
+++ __test__/sqlmethods-create.test.js
@@ -1,13 +1,9 @@
 import { mapInsertNewEntityToSQLStatement } from '../src/sqlmethods/';
 
-tableName, values, keys;
-
 const tableName = 'Pokemon';
 const values = ['pikachu', 100];
 const keys = ['name', 'powerlevel'];
-
 const valuesMapped = values.map(item => `'${item}'`);
-
 const result = `INSERT INTO ${tableName} (${keys.join()}) VALUES (${valuesMapped});`;
 
 test('Use mapInsertNewEntityToSQLStatement to create sql insert statement', () => {
