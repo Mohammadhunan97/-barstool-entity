da50a26ea3b9aebf0082ef7d7aa63098036525d4
moe c <mchugtai@guildmortgage.net>
2019-01-09 00:35:49 -0800

    added contingencies for create

--- src/routegenerators/create.js
+++ src/routegenerators/create.js
@@ -8,14 +8,27 @@ const createOneItemRoute = (app, table, connection) => {
   //   description: `creates a new item in ${table.tableName} table`
   // });
   app.post(`/${table.tableName}/new`, (req, res) => {
-    const values = Object.values(req.body);
+    const requestValues = Object.values(req.body);
     const requestKeys = Object.keys(req.body);
     const keys = table.columns.map(item => item.columnName); // barstool keys
     const missingKeys = [];
+    const extraKeys = [];
+    const validKeys = [];
+    const validValues = [];
 
     keys.forEach(barstoolKey => {
       if (!requestKeys.includes(barstoolKey)) {
         missingKeys.push(barstoolKey);
+      }
+    });
+
+    requestKeys.forEach((reqKey, i) => {
+      if (keys.includes(reqKey)) {
+        // if the request key is infact a key we need, we will need to put that key and its value in the respective 'validKeys' and 'validValues' arrays.
+        validValues.push(requestValues[i]);
+        validKeys.push(reqKey);
+      } else {
+        extraKeys.push(reqKey);
       }
     });
 
@@ -25,10 +38,26 @@ const createOneItemRoute = (app, table, connection) => {
         missingRequestValues: missingKeys
       });
     } else {
-      const sqlStatement = mapInsertNewEntityToSQLStatement(table.tableName, values, requestKeys);
+      const sqlStatement = mapInsertNewEntityToSQLStatement(
+        table.tableName,
+        validValues,
+        validKeys
+      );
       connection.query(sqlStatement, (err, rows) => {
         if (err) res.send(err);
-        res.status(200).send(`Successfully inserted new item in ${table.tableName} table`);
+        if (extraKeys.length > 0) {
+          res.json({
+            successFullyInsertedItem: true,
+            invalidData: extraKeys,
+            insertedDataFromValidRequest: validKeys,
+            message: `Successfully inserted new item in ${table.tableName} table`
+          });
+        } else {
+          res.json({
+            successFullyInsertedItem: true,
+            message: `Successfully inserted new item in ${table.tableName} table`
+          });
+        }
       });
     }
   });
